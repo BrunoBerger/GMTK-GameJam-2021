d470dcd01f2414428d291599b66bfb9062be3d07
BrunoBerger <bruno.berger@stud.hshl.de>
2021-06-12 18:49:13 +0200

    base rolling and collision

--- Assets/Scripts/RollingMovement.cs
+++ Assets/Scripts/RollingMovement.cs
@@ -7,8 +7,13 @@ public class RollingMovement : MonoBehaviour
     Vector2 userInput;
     Rigidbody2D rb;
 
-    public float movSpeed = 0.15f;
-    public float jumpStr = 10f;
+    public bool isGrounded;
+    public float currentRotation;
+    public float rollPower;
+    public float maxSpeed;
+    public float jumpStr;
+
+    public Stack<Rigidbody2D> Limbs;
 
     // Start is called before the first frame update
     void Start()
@@ -24,10 +29,56 @@ public class RollingMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        currentRotation = (Mathf.Abs(rb.angularVelocity));
         // Horizontal Movement
-        userInput.Set(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        rb.AddRelativeTour
-        // Debug.DrawRay(transform.position, userInput*2, Color.red);
+        userInput.Set(Input.GetAxis("Horizontal")*-1, Input.GetAxis("Vertical"));
+        // Debug.DrawRay(transform.position, userInput*10f, Color.red);
+
+
+        if (isGrounded)
+        {
+            if (currentRotation < maxSpeed)
+            {
+                rb.AddTorque(userInput.x*rollPower, ForceMode2D.Force);
+            }
+            if (Input.GetButton("Jump"))
+            {
+                rb.AddForce(new Vector2(0, jumpStr));
+            }
+        }
+    }
+
+    void AttachNewLimb(Collision2D collision)
+    {
+        Rigidbody2D newRB = collision.collider.attachedRigidbody;
+        Limbs.Push(newRB);
+    }
+
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.name == "Ground")
+        {
+            //Output the message
+            print("Ground touch");
+            isGrounded = true;
+        }
+        if (collision.gameObject.tag == "Limb")
+        {
+            AttachNewLimb(collision);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        
+        if (collision.collider.name == "Ground")
+        {
+            //Output the message
+            print("Ground left");
+            isGrounded = false;
+        }
     }
 }
 
